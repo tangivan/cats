341729844fd761d223c5d65e3f9ede75cff41fee
Ivan <ivanxintang@gmail.com>
2021-11-02 17:24:02 -0700

    wrote tests for the cat breeds files!

---
+++ asyncBreeds.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, functionToRunWhenThingsAreDone) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) functionToRunWhenThingsAreDone(data)
+    if (error) functionToRunWhenThingsAreDone(undefined);
+  });
+};
+
+module.exports = breedDetailsFromFile;
---
+++ test/syncBreedsTest.js
@@ -0,0 +1,13 @@
+const assert = require('chai').assert;
+const breedDetails = require('../syncBreeds');
+
+describe('#breedDetails', () => {
+  it('returns breed details for the Bombay breed', () => {
+    // We expect it to be a function, but will it be?
+    // get the return value right away from the function
+    const bombay = breedDetails('Bombay');
+    console.log(bombay); //=> prints out the description for that breed
+    const expectedDesc = "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder.";
+    assert.equal(expectedDesc, bombay);
+  });
+});
